18e0459e8feb1ff7738437730a33f630a4167d90
Max <max.wolts@gmail.com>
2023-06-30 18:34:14 -0300

    :heavy_plus_sign: added express

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,1 +1,12 @@
-console.log("Hola mundo desde el server")
+import express from "express"
+import type { Express, Request, Response } from "express"
+import { config } from "./config"
+
+const app: Express = express()
+app.get('*', (req: Request, res: Response) => {
+    res.send(`<h1>Heola mundo con ruta: ${req.url}</h1>`)
+})
+
+app.listen(config.PORT, () => {
+    console.log(`Listening in http://localhost:${config.PORT}`)
+})
