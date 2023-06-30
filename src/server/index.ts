33f20c7c56fa4fc043e719df56bc02f862b997c8
Max <max.wolts@gmail.com>
2023-06-30 20:56:48 -0300

    :sparkles: rendering react from server

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,10 +2,11 @@ import express from "express"
 import type { Express, Request, Response } from "express"
 import { config } from "./config"
 import { template } from "./render/template"
+import { render } from "./render"
 
 const app: Express = express()
 app.get('*', (req: Request, res: Response) => {
-    res.send(template(`<h1>Hola desde la ruta ${req.url}</h1>`))
+    res.send(template(render(req.url)))
 })
 
 app.listen(config.PORT, () => {
