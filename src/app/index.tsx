ae9c0ffdb0d0dc16fbdb9d413f2dfd1284413cdf
Max <max.wolts@gmail.com>
2023-07-04 15:42:39 -0300

    :sparkles: consuming initial props

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -8,8 +8,19 @@ import './assets/favicon.ico'
 const container = document.getElementById('app')
 // const root = createRoot(container)
 
+/*
+La propiedad __INITIAL_PROPS__ no es reconocida como parte del objeto window en TypeScript
+porque esta propiedad no es parte del estándar de JavaScript ni del navegador.
+
+En Next.js, __INITIAL_PROPS__ es una convención utilizada por el servidor para pasar datos
+iniciales al cliente cuando se realiza el renderizado inicial del lado del servidor (SSR).
+Es una forma de transferir datos desde el servidor al cliente antes de que la aplicación React
+se cargue completamente en el navegador.
+*/
+const initialProps = window.__INITIAL_PROPS__
+
 hydrateRoot( container,
     <BrowserRouter>
-        <App />
+        <App {...initialProps} />
     </BrowserRouter>
 )
