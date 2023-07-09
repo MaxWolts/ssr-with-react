b0808bc57c8f4562bdf437a6f1d953bac97d1e44
Max <max.wolts@gmail.com>
2023-07-08 23:21:11 -0300

    :lipstick: GalaxyCard styles changed

--- src/app/components/GalaxyCard/styles.ts
+++ src/app/components/GalaxyCard/styles.ts
@@ -4,6 +4,13 @@ export const GalaxyCardContainer = styled.div`
   box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
   border-radius: 8px;
   color: #f9f9f9;
+  padding: 0 1rem;
+  background: rgba(28, 94, 198, 0.2);
+  border-radius: 16px;
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(5px);
+  -webkit-backdrop-filter: blur(5px);
+  border: 1px solid rgba(28, 94, 198, 0.3);
   
   & > .title {
     font-size: 2rem;
@@ -15,7 +22,7 @@ export const GalaxyCardContainer = styled.div`
     width: 100%;
     height: 240px;
     object-fit: cover;
-    
+    border-radius: 5%;
   }
 
   & > .info-container {
